d0142e74caa3a3755938b4ac022e065bba8919ec
xMohnad <mohnad00967@gmail.com>
2025-01-07 19:44:22 +0000

    Updated the completion commands for pkg.

--- config/fish/completions/pkg.fish
+++ config/fish/completions/pkg.fish
@@ -1,13 +1,41 @@
-complete -c pkg -n '__fish_use_subcommand' -f -a "--check-mirror" -d "Force a re-check of availability of mirrors"
-complete -c pkg -n '__fish_use_subcommand' -f -a "autoclean" -d "Remove outdated packages from the cache"
-complete -c pkg -n '__fish_use_subcommand' -f -a "clean" -d "Remove all packages from the cache"
-complete -c pkg -n '__fish_use_subcommand' -f -a "files" -d "Show all files installed by packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "install" -d "Install specified packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "list-all" -d "List all packages available in repositories"
-complete -c pkg -n '__fish_use_subcommand' -f -a "list-installed" -d "List installed packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "reinstall" -d "Reinstall specified packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "search" -d "Search packages by query"
-complete -c pkg -n '__fish_use_subcommand' -f -a "show" -d "Show metadata for specified packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "uninstall" -d "Uninstall specified packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "upgrade" -d "Upgrade all installed packages"
-complete -c pkg -n '__fish_use_subcommand' -f -a "update" -d "Update package manager databases"
+function get_cache_size
+  set cache_dir "/data/data/com.termux/cache/apt/archives"
+  set cache_size (du -sh "$cache_dir" 2>/dev/null | cut -f1)
+  
+  if test -n "$cache_size"
+    echo "Cache size: $cache_size"
+  else
+    echo "Cache size: 0B"
+  end
+end
+
+function __fish_pkg_filter_output
+    awk -F'/' '{print $1}' | grep -v '^Listing'
+end
+
+function __fish_pkg_packages
+    pkg list-all 2>/dev/null | __fish_pkg_filter_output
+end
+
+function __fish_pkg_installed_packages
+    pkg list-installed 2>/dev/null | __fish_pkg_filter_output
+end
+
+function __fish_pkg_commands
+  echo -e "install\tinstall specified packages"
+  echo -e "upgrade\tUpgrade all installed packages"
+  echo -e "update\tUpdate package manager databases"
+  echo -e "search\tSearch packages by query"
+  echo -e "uninstall\tUninstall specified packages"
+  echo -e "autoclean\tRemove outdated packages from the cache"
+  echo -e "clean\tRemove all packages from cache. "(get_cache_size)
+  echo -e "files\tShow all files installed by packages"
+  echo -e "list-all\tlist all packages available in repositories"
+  echo -e "list-installed\tList installed packages"
+  echo -e "reinstall\tReinstall specified packages"
+end
+
+complete -c pkg -n "__fish_use_subcommand" -x -a "(__fish_pkg_commands)"
+complete -c pkg -l check-mirror -d "Force a re-check of availability of mirrors"
+complete -c pkg -n "__fish_seen_subcommand_from install search show" -x -a "(__fish_pkg_packages)"
+complete -c pkg -n "__fish_seen_subcommand_from uninstall reinstall files" -x -a "(__fish_pkg_installed_packages)"
