d38acf8ce387e69e85ff5b64f44dce7071e2abb5
xMohnad <mohnad00967@gamil.com>
2025-06-11 17:52:05 +0300

    Add Dart language server support

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,31 +1,32 @@
 local options = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		css = { "prettier" },
-		html = { "djlint" },
-		json = { "prettier" },
-		javascript = { "prettier" },
-		python = {
-			-- To fix auto-fixable lint errors.
-			"ruff_fix",
-			-- To run the Ruff formatter.
-			"ruff_format",
-			-- To organize the imports.
-			"ruff_organize_imports",
-		},
-		bash = { "shfmt" },
-		markdown = { "mdformat" },
-		yaml = { "prettier" },
-		yml = { "prettier" },
-		tcss = { "prettier" },
-		go = "gofumpt",
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    javascript = { "prettier" },
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
+    bash = { "shfmt" },
+    markdown = { "mdformat" },
+    yaml = { "prettier" },
+    yml = { "prettier" },
+    tcss = { "prettier" },
+    go = { "gofumpt" },
+    dart = { "dart_format" },
+  },
 
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 5000,
-		lsp_format = "fallback",
-	},
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 5000,
+    lsp_format = "fallback",
+  },
 }
 
 return options
