6a93173259c311d5d364d9dd7ad894f24ee2f8ac
xMohnad <mohnad00967@gmail.com>
2026-04-02 02:47:11 +0300

    refactor(fish): cleanup alias

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -3,7 +3,6 @@ alias py="python"
 alias pt="ptpython"
 alias q="exit"
 alias c="clear"
-alias cn="clear; neofetch"
 alias pg="ping google.com"
 alias b="cd -"
 
@@ -18,7 +17,6 @@ alias lt="eza --icons --tree --group-directories-first --time-style=long-iso"
 alias lta="eza --icons --tree -lgha --group-directories-first --time-style=long-iso"
 alias bat="bat --theme OneHalfDark --style=header,numbers"
 alias cat="bat --theme OneHalfDark -p (string join ' ' $argv)"
-alias cat="bat --theme OneHalfDark -p (string join ' ' $argv)"
 alias disk='df -kh . | awk "NR==2 {used=\$3; total=\$2; free=\$4; percent=\$5; printf \"%s / %s = %s (%s)\\n\", used, total, free, percent}"'
 
 # Colorize grep output (good for log files)
@@ -27,24 +25,4 @@ alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
 # Git Commands
-alias gitgraph="git log --all --decorate --oneline --graph"
-alias gitupdate="git fetch && git pull"
 alias gits="git status"
-
-# Terminal and System Settings
-alias unsource="exec \$SHELL -l" # Reload shell
-
-# navigation
-alias ...='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
-
-# termbin
-alias tb="nc termbin.com 9999"
-
-# the terminal rickroll
-alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
-
-# silicon
-alias silicon-font="silicon --font 'DejaVu Sans Mono'"
