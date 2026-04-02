65e0f959ceea945b61aae9c843a96e2f86ab1467
xMohnad <mohnad00967@gmail.com>
2026-04-02 10:59:58 +0300

    refactor(bat): use config instead of fish aliases

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -15,8 +15,6 @@ alias ls="eza --icons --group-directories-first --time-style=long-iso"
 alias la="eza --icons -lgha --group-directories-first --time-style=long-iso"
 alias lt="eza --icons --tree --group-directories-first --time-style=long-iso"
 alias lta="eza --icons --tree -lgha --group-directories-first --time-style=long-iso"
-alias bat="bat --theme OneHalfDark --style=header,numbers"
-alias cat="bat --theme OneHalfDark -p (string join ' ' $argv)"
 alias disk='df -kh . | awk "NR==2 {used=\$3; total=\$2; free=\$4; percent=\$5; printf \"%s / %s = %s (%s)\\n\", used, total, free, percent}"'
 
 # Colorize grep output (good for log files)
