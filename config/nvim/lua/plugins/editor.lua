f610191959926a400bfe0e1a731a19fb2fff5ddf
xMohnad <mohnad00967@gmail.com>
2026-04-20 01:47:18 +0300

    refactor(editor): change TSJToggle mapping to <leader>j

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -113,7 +113,7 @@ return {
   {
     "Wansmer/treesj",
     keys = {
-      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+      { "<leader>j", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
     },
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
